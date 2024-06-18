55d73d41d6fc19f81b57fc27adf1ac569bbbbc68
Ehoneah Frank <ehoneahfrank2017@gmail.com>
2024-06-18 13:30:42 +0000

    Personal Portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('btnn')
+// console.log(btn)
+btn.onclick = function(){
+    alert('More Projects underway!!!')
+    // console.log ('button has been clicked')
+}
